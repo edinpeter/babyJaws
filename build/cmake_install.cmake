dc5f07e6a17aceee059e582107fc34229a913aee
chilypepper <chilypepper@github.com>
2015-09-18 15:45:09 -0400

    added launch file for one stop shopping

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unsp
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   INCLUDE("/home/peter/babyJaws/build/gtest/cmake_install.cmake")
+  INCLUDE("/home/peter/babyJaws/build/launch/cmake_install.cmake")
   INCLUDE("/home/peter/babyJaws/build/rov/cmake_install.cmake")
 
 ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
---
+++ build/launch/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/peter/babyJaws/src/launch
+# Build directory: /home/peter/babyJaws/build/launch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(_ctest_launch_roslaunch-check_babyJaws.launch "/home/peter/babyJaws/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/jade/share/catkin/cmake/test/run_tests.py" "/home/peter/babyJaws/build/test_results/launch/roslaunch-check_babyJaws.launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/peter/babyJaws/build/test_results/launch" "/opt/ros/jade/share/roslaunch/cmake/../scripts/roslaunch-check -o '/home/peter/babyJaws/build/test_results/launch/roslaunch-check_babyJaws.launch.xml' '/home/peter/babyJaws/src/launch/babyJaws.launch' ")
